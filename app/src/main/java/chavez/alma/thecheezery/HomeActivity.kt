6ff50976c47d095588a594e16710c1de638a9dbb
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-03-12 00:48:58 -0700

    Correct version

---
+++ app/src/main/java/chavez/alma/thecheezery/HomeActivity.kt
@@ -0,0 +1,12 @@
+package chavez.alma.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
